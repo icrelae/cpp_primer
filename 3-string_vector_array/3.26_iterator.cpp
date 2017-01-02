c77bf763dee6aba45ebc272ece588eaae9de0bf2
icrelae <icrelae@gmail.com>
2017-01-02 22:48:03 +0000

    base of text_query

--- 3-string_vector_array/3.26_iterator.cpp
+++ 3-string_vector_array/3.26_iterator.cpp
@@ -1,4 +1,17 @@
-/* 迭代器重载了两参数为迭代器的减号运算符，
+/* 2017.01.02 18:15
  * P_101
+ * 迭代器重载了两参数为迭代器的减号运算符，
  * 但是没有重载过两参数为迭代器的加号运算符。
  */
+ */
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+	return 0;
+}
