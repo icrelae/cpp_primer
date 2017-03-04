8712eb22f888a777e8c791389cb1f1beda0c237f
icrelae <icrelae@gmail.com>
2017-03-04 22:14:53 +0000

    text query

---
+++ 15-object-oriented_programming/15.31-text_query.cpp
@@ -0,0 +1,19 @@
+/* 2017.03.04 22:11
+ * P_566
+ *
+ * a: s1 | s2 & !s3
+ * b: s1 | (s2 & !s3)
+ * c: s1 & s2 | (s3 & s4)
+ */
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+	return 0;
+}
+
