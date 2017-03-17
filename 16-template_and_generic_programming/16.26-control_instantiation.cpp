f8d5611dc3dec7d8f710cf5ff0f3fdf8d87f7d94
icrelae <icrelae@gmail.com>
2017-03-17 10:27:40 +0000

    control instantiation

---
+++ 16-template_and_generic_programming/16.26-control_instantiation.cpp
@@ -0,0 +1,19 @@
+/* 2017.03.17 08:33
+ * P_599
+ * !!!
+ * Assuming NoDefault is a class that does not have a default constructor, can
+ * we explicitly instantiate vector<NoDefault>? If not, why not?
+ * http://stackoverflow.com/questions/21525169/while-explicitly-instantiating-vectorsometype-what-is-the-sometype-default-co
+ */
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+	return 0;
+}
+
